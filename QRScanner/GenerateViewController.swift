03ccac82dbfef3b9864da9a9bbdf83ec695cfcd0
Nghia Nguyen <quangnghiadev@gmail.com>
2018-06-17 08:45:27 +0700

    ver 1

---
+++ QRScanner/GenerateViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GenerateViewController.swift
+//  QRScanner
+//
+//  Created by Nghia Nguyen on 6/16/18.
+//  Copyright © 2018 Nghia Nguyen. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import RSBarcodes_Swift
+
+class GenerateViewController: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func generateCode(input: String) {
+        
+    }
+}
---
+++ QRScanner/RecognizeViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by Nghia Nguyen on 6/16/18.
+//  Copyright © 2018 Nghia Nguyen. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecognizeViewController: UIViewController {
+
+    var captureSession: AVCaptureSession!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    var isShowingResult = false
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        captureSession = AVCaptureSession()
+        if initCamera() {
+            setMetadataOutput()
+            addCameraLayer()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if !captureSession.isRunning {
+            captureSession.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if captureSession.isRunning {
+            captureSession.stopRunning()
+        }
+    }
+    
+    func initCamera() -> Bool {
+        guard let captureDevice = AVCaptureDevice.default(for: .video)  else {
+            print("Failed to get the camera device")
+            return false
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+                return true
+            }
+        } catch {
+            print("\(error)")
+        }
+        return false
+    }
+    
+    func setMetadataOutput() {
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        if captureSession.canAddOutput(captureMetadataOutput) {
+            captureSession.addOutput(captureMetadataOutput)
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: .main)
+            autoreleasepool {
+                let types = NSMutableArray(array: captureMetadataOutput.availableMetadataObjectTypes)
+                types.remove(AVMetadataObject.ObjectType.dataMatrix)
+                types.remove(AVMetadataObject.ObjectType.face)
+                captureMetadataOutput.metadataObjectTypes = NSArray(array: types) as! [AVMetadataObject.ObjectType]
+            }
+        }
+    }
+    
+    func addCameraLayer() {
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer)
+    }
+    
+    func showResult(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let copyAction = UIAlertAction(title: "Copy", style: .default, handler: { [unowned self] (action) in
+            UIPasteboard.general.string = message
+            self.view.makeToast("Copied")
+            self.isShowingResult = false
+        })
+        alertVC.addAction(copyAction)
+        alertVC.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { [unowned self] _ in
+            self.isShowingResult = false
+        }))
+        alertVC.preferredAction = copyAction
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+    func processData(data: AVMetadataMachineReadableCodeObject) {
+        if data.stringValue != nil {
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            isShowingResult = true
+            let type = data.type.rawValue.split(separator: ".")
+            let barcodeType = String(type.last ?? "Not found")
+            showResult(title: barcodeType, message: data.stringValue!)
+        }
+    }
+}
+
+extension RecognizeViewController: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if isShowingResult {
+            return
+        }
+        guard metadataObjects.count > 0, let metaDataObj = metadataObjects[0] as? AVMetadataMachineReadableCodeObject else {
+            return
+        }
+        processData(data: metaDataObj)
+    }
+}
+
